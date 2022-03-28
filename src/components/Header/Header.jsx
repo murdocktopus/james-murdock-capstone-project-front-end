d221bbe62acaf7dc20d4a09abd0f4d3e7c9b2227
James Murdock <james.k.murdock@gmail.com>
2022-03-28 03:44:02 -0700

    Header Styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,68 @@
 import "./Header.scss";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Component } from "react";
+import iconHome from "../../assets/images/home.svg";
+import iconGroup from "../../assets/images/group.svg";
+import iconComment from "../../assets/images/comment.svg";
+import iconBell from "../../assets/images/bell.svg";
+import iconAvatar from "../../assets/images/avatar-user-profile.svg";
 
 class Header extends Component {
   render() {
     return (
-      <div className="header-container">
-        <header className="header"></header>
-      </div>
+      <header className="header">
+        <div className="header__container">
+          <Link to="/" className="header__nav-link">
+            <logo className="logo">scrollmate</logo>
+          </Link>
+
+          <div className="header__nav-item header__nav-item--hidden">
+            <Link to="/" className="header__nav-link">
+              <img src={iconHome} alt="Home_Icon" className="icon" />
+              <p className="icon__label icon__label--hidden">Home</p>
+            </Link>
+          </div>
+
+          <div className="header__nav-item header__nav-item--hidden">
+            <Link to="/" className="header__nav-link">
+              <img src={iconGroup} alt="Groups_Icon" className="icon" />
+              <p className="icon__label icon__label--hidden">Groups</p>
+            </Link>
+          </div>
+
+          <div className="header__nav-item header__nav-item--hidden">
+            <Link to="/" className="header__nav-link">
+              <img src={iconComment} alt="Comment_Icon" className="icon" />
+              <p className="icon__label icon__label--hidden">Comment</p>
+            </Link>
+          </div>
+
+          <div className="header__nav-item">
+            <Link to="/" className="header__nav-link">
+              <img src={iconBell} alt="Notifications_Icon" className="icon" />
+              <p className="icon__label icon__label--hidden">Notifications</p>
+            </Link>
+          </div>
+
+          <form action="" className="search search--tablet-display">
+            <input
+              type="search"
+              className="search__input"
+              placeholder="Search..."
+              name="search"
+              id="site-search"
+            />
+            <input type="submit" hidden />
+          </form>
+
+          <div className="header__nav-item header__nav-item--hidden">
+            <Link to="/" className="header__nav-link">
+              <img src={iconAvatar} alt="Profile_Icon" className="icon" />
+              <p className="icon__label icon__label--hidden">Profile</p>
+            </Link>
+          </div>
+        </div>
+      </header>
     );
   }
 }
